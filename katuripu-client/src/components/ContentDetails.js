d5415a2f54027809df3803cc788bfdc4c6ca41a8
AbdessamadAe <ab.aitelmouden@gmail.com>
2024-01-03 12:15:10 +0100

    Linked Content Page to Home Page

--- katuripu-client/src/components/ContentDetails.js
+++ katuripu-client/src/components/ContentDetails.js
@@ -2,18 +2,29 @@ import React from 'react'
 
 export const ContentDetails = ({ content }) => {
     return (
-        <div className='m-auto'>
-            {content?.map((content) => (
-            <div className='font-amiri text-right flex flex-col items-end'>
-                <h1 className=''>تعريف الإشتقاق</h1>
-                <div></div>
-                <iframe className='m-auto mt-8 mb-8'
-                    title='Youtube player'
-                    sandbox='allow-same-origin allow-forms allow-popups allow-scripts allow-presentation'
-                    src={`https://youtube.com/embed/${content.content_video_link}?autoplay=0`}>
-                </iframe> 
+        <div className='pt-[80px]'>
+            <div className='ml-[60px] mr-[60px] space-y-8'>
+                {content?.map((content) => (
+                    <div className=' mr-[140px] ml-[140px] space-y-6 font-amiri text-right flex flex-col items-end'>
+                        <h1 className='mr-[12px] text-h3'>{content.title}</h1>
+                        <div className='mr-[12px] text-h6 text-[#747677]'>
+                            {content.content_body}
+                        </div>
+                        <div className="">
+                            <iframe
+                                width="853"
+                                height="480"
+                                src={`https://www.youtube.com/embed/${content.content_video_link}`}
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                                title="Embedded youtube"
+                            />
+                        </div>
+
+                    </div>
+
+                ))}
             </div>
-            ))}
         </div>
     )
 }
