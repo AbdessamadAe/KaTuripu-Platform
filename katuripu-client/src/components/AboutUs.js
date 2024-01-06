a76f2b6745c97c49a3b6fef1edd1bb23178b6402
AbdessamadAe <ab.aitelmouden@gmail.com>
2024-01-06 21:38:26 +0100

    added about us section

---
+++ katuripu-client/src/components/AboutUs.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Nav from './Nav'
+import { Footer } from './Footer'
+import team from '../images/team.PNG'
+
+
+export const AboutUs = () => {
+    return (
+        <div>
+            <Nav />
+            <div className=' animate-slide-fade-in mt-[80px] p-8 md:px-6'>
+                
+            </div>
+            <Footer />
+        </div>
+    )
+}
