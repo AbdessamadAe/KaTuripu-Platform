2963d2eed15e7103942913084a81b6cb108338b9
AbdessamadAe <ab.aitelmouden@gmail.com>
2025-04-17 22:51:55 +0200

    contact-us tranlsation

--- frontend/src/app/[locale]/contact-us/page.tsx
+++ frontend/src/app/[locale]/contact-us/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { useTranslations } from 'next-intl'
 
 const ContactUs = () => {
+  const t = useTranslations('contact-us')
   return (
     <div>
       <div className='flex flex-col sm:grid sm:grid-flow-col sm:grid-cols-2 items-center mt-28 sm:mt-[80px] sm:pt-0 sm:p-6'>
         <div className="relative my-auto mx-auto max-w-[80%] bg-transparent font-amiri">
-          <h1 className="text-3xl text-[#333] font-semibold text-center">تواصل معنا</h1>
+          <h1 className="text-3xl text-[#333] font-semibold text-center">{t('contact')}</h1>
           <form
             className="mt-8 space-y-4"
             action="https://formsubmit.co/ssamad.2r@gmail.com"
@@ -14,26 +16,26 @@ const ContactUs = () => {
             <input
               type="text"
               name="name"
-              placeholder="Nom"
+              placeholder={t('form.name')}
               required
               className="w-full rounded-md py-3 px-4 bg-gray-100 text-sm outline-blue-500"
             />
             <input
               type="email"
               name="email"
-              placeholder="Email"
+              placeholder={t('form.email')}
               required
               className="w-full rounded-md py-3 px-4 bg-gray-100 text-sm outline-blue-500"
             />
             <input
               type="text"
               name="subject"
-              placeholder="Sujet"
+              placeholder={t('form.subject')}
               className="w-full rounded-md py-3 px-4 bg-gray-100 text-sm outline-blue-500"
             />
             <textarea
               name="message"
-              placeholder="Message"
+              placeholder={t('form.message')}
               rows={6}
               required
               className="w-full rounded-md px-4 bg-gray-100 text-sm pt-3 outline-blue-500"
@@ -42,7 +44,7 @@ const ContactUs = () => {
               type="submit"
               className="text-neutral-50 bg-[#A78BFA] hover:bg-blue-600 font-semibold rounded-md text-sm px-4 py-3 w-full"
             >
-              Envoyer
+              {t('form.submit')}
             </button>
           </form>
         </div>
