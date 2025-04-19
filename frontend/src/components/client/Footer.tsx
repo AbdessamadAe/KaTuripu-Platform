c8b59e9d622bdc9cd5aa24a7ac6b7db3bd1d3fb8
AbdessamadAe <ab.aitelmouden@gmail.com>
2025-04-19 20:05:58 +0200

    footer styling

--- frontend/src/components/client/Footer.tsx
+++ frontend/src/components/client/Footer.tsx
@@ -4,33 +4,33 @@ import { FaFacebook, FaInstagram, FaTiktok, FaYoutube, FaLinkedin } from "react-
 export const Footer: React.FC = () => {
     const d = new Date();
     return (
-        <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
-            <div className="flex justify-center mt-8 space-x-6">
-                <a href="https://www.youtube.com/@KaTuripu" className="text-gray-400 hover:text-gray-500">
-                    <FaYoutube className='w-6 h-6' />
-                </a>
-                <a href="https://www.linkedin.com/company/katuripu" className="text-gray-400 hover:text-gray-500">
-                    <FaLinkedin className='w-6 h-6' />
-                </a>
-                <a href="https://www.facebook.com/KaTuriipu/" className="text-gray-400 hover:text-gray-500">
-                    <FaFacebook className='w-6 h-6' />      
-                </a>
-                <a href="https://www.instagram.com/katuriipu/" className="text-gray-400 hover:text-gray-500">
-                    <FaInstagram className='w-6 h-6' />
-                </a>
-                <a href="https://www.tiktok.com/@katuripu?lang=en" className="text-gray-400 hover:text-gray-500">
-                    <FaTiktok className='w-6 h-6' />
-                </a>
-            </div>
-            <div className="flex flex-wrap justify-between items-center mt-8 text-gray-400">
+        <div className="max-w-screen-xl px-4 py-6 mx-auto overflow-hidden sm:px-6 lg:px-8">
+            <div className=" space-y-2 text-center text-gray-400">
                 <p className="text-base leading-6 text-gray-400">
                     © {d.getFullYear()} KaTuripu | All Rights Reserved
                 </p>
+                {/* <div className="justify-center space-x-6 hidden md:flex">
+                    <a href="https://www.youtube.com/@KaTuripu" className="text-gray-400 hover:text-gray-500">
+                        <FaYoutube className='w-6 h-6' />
+                    </a>
+                    <a href="https://www.linkedin.com/company/katuripu" className="text-gray-400 hover:text-gray-500">
+                        <FaLinkedin className='w-6 h-6' />
+                    </a>
+                    <a href="https://www.facebook.com/KaTuriipu/" className="text-gray-400 hover:text-gray-500">
+                        <FaFacebook className='w-6 h-6' />
+                    </a>
+                    <a href="https://www.instagram.com/katuriipu/" className="text-gray-400 hover:text-gray-500">
+                        <FaInstagram className='w-6 h-6' />
+                    </a>
+                    <a href="https://www.tiktok.com/@katuripu?lang=en" className="text-gray-400 hover:text-gray-500">
+                        <FaTiktok className='w-6 h-6' />
+                    </a>
+                </div> */}
                 <p className="text-sm leading-6 hover:text-gray-300 transition-colors duration-300">
                     <span className="font-light italic">Developed by </span>
-                    <a 
-                        href="https://github.com/abdessamadae" 
-                        target="_blank" 
+                    <a
+                        href="https://github.com/abdessamadae"
+                        target="_blank"
                         rel="noopener noreferrer"
                         className="font-medium hover:text-indigo-400"
                     >
